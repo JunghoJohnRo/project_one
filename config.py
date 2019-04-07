ea3389f58eea36a1d0171a4a20e033ed311e335c
Kenny Daniels <kennydumn@gmail.com>
2019-04-06 21:58:00 -0500

    Added Iowa API

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-gkey = "AIzaSyA-jQNaSUIWVVTiNn-dYgMzJdMOkcWU1_4"
-api_key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
+Iowa_Key = "St1t3F6XuHyRioRiDutVeGV21"
+Census_Key = "85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
